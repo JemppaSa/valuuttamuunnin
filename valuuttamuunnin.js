a374799b7f0800e9328002c085d17a8170a2aafc
JemppaSa <jemppa.sa@gmail.com>
2024-01-31 19:56:00 +0200

    Laskenta funktion kaava ja lasku nappi toimimaan

--- valuuttamuunnin.js
+++ valuuttamuunnin.js
@@ -1,22 +1,28 @@
+let data
 
 async function fetchData() {
-
     console.log("Fetching data!");
-    
-
     try {
         const response = await fetch("https://open.er-api.com/v6/latest/USD", {
             method: 'GET',
-            
         });
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-
-        const data = await response.json();
+        data = await response.json();
         console.log(data);
-        
     } catch (err) {
         console.error(err.message);
     }
 }
+function laske() {
+    const currencyFrom = document.getElementById("currencyFrom")
+    const currencyTo = document.getElementById("currencyTo")
+    const fromRatio = data.rates[currencyFrom.value]
+    const toRatio = data.rates[currencyTo.value]
+    const value = document.getElementById("value").value
+
+    let valueDollar = value/fromRatio;
+    document.getElementById("result").value = (valueDollar*toRatio).toFixed(2)
+}
+//fetchData()
